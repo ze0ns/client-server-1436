9cb0f6565204856c1aa983f821d8358e87c918e9
ze0ns <zeons@mail.ru>
2021-08-15 09:04:39 +0300

    сделал переход между контроллерами через сигвей. Кодом не работает

---
+++ client-server-1436/lesson2/ChooseFruitViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ChooseFruitViewController.swift
+//  client-server-1436
+//
+//  Created by zeons on 15.08.2021.
+//
+
+import UIKit
+
+class ChooseFruitViewController: UIViewController {
+
+    @IBOutlet var fruitLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+
+    
+//    @IBAction func choiseFruitAction(_ sender: Any) {
+//        let displayFruitsVC = DisplayFruitsTableViewController()
+//
+//        navigationController?.pushViewController(displayFruitsVC, animated: true)
+//
+//    }
+    
+}
---
+++ client-server-1436/lesson2/DisplayFruitsTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DisplayFruitsTableViewController.swift
+//  client-server-1436
+//
+//  Created by zeons on 15.08.2021.
+//
+
+import UIKit
+
+class DisplayFruitsTableViewController: UITableViewController {
+    let fruits = ["Авокадо","Банан","Яблоко","Киви"]
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return fruits.count
+    }
+
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = fruits[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+           let fruit = fruits[indexPath.row]
+           
+       }
+
+
+}
