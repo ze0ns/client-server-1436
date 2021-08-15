4bafb3ea1c9eae4ea76c81e6690307779257dbaa
ze0ns <zeons@mail.ru>
2021-08-15 12:54:19 +0300

    JSON, NetworkController

---
+++ client-server-1436/lesson2/NetworkViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkViewController.swift
+//  client-server-1436
+//
+//  Created by zeons on 15.08.2021.
+//
+
+import UIKit
+
+class NetworkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstRequest()
+    }
+
+    func firstRequest(){
+        guard let url = URL(string: "http://samples.openweathermap.org/data/2.5/forecast?q=Moscow,DE&appid=b1b15e88fa797225412429c1c50c122a1") else {return}
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url) {(data, response, error) in
+        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+            print(json)
+        }
+    }
+    func secondRequest(){
+        
+    }
+    
+    func thirdRequest(){
+        
+    }
+    
+}
