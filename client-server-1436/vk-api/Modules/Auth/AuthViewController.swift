46adcd57f3b8b3ed014d25edef9bdc21a1efe45a
zeons <oao@i-cio.ru>
2021-08-17 16:05:26 +0300

    add params and token

--- client-server-1436/vk-api/Modules/Auth/AuthViewController.swift
+++ client-server-1436/vk-api/Modules/Auth/AuthViewController.swift
@@ -11,24 +11,31 @@ import WebKit
 class AuthViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    {
+            didSet{
+                webView.navigationDelegate = self
+            }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        authorizeToVK()
     }
     
     func authorizeToVK(){
+        //Позволяет запрос разить, и позволят сделать запрос в UTF-8
         var urlComponents = URLComponents()
                 urlComponents.scheme = "https"
                 urlComponents.host = "oauth.vk.com"
                 urlComponents.path = "/authorize"
                 urlComponents.queryItems = [
-                    URLQueryItem(name: "client_id", value: "1234567"),
+                    URLQueryItem(name: "client_id", value: "7928655"),
                     URLQueryItem(name: "display", value: "mobile"),
                     URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
                     URLQueryItem(name: "scope", value: "262150"),
                     URLQueryItem(name: "response_type", value: "token"),
+                    URLQueryItem(name: "revoke", value: "1"),
                     URLQueryItem(name: "v", value: "5.68")
                 ]
                 
@@ -38,4 +45,38 @@ class AuthViewController: UIViewController {
     }
 
 
+}
+extension AuthViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        //Забрали УРЛ
+        guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment  else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        //Разбили на фрагменты
+        //Вырезали токен доступа
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        guard let token = params["access_token"] else {return}
+        
+        print(token)
+        
+        Session.shared.token = token
+        
+        let frendsVC = DisplayFrendsTableViewController()
+        navigationController?.pushViewController(frendsVC, animated: true)
+        
+        
+        decisionHandler(.cancel)
+    }
 }
