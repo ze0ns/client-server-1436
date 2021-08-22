9e87db64ce2e8e7781f0a0534a613fcae4736ab9
ze0ns <zeons@mail.ru>
2021-08-22 20:26:20 +0300

    изменил параметры сессии для доступа к АПИ ВК

--- client-server-1436/vk-api/Modules/Auth/AuthViewController.swift
+++ client-server-1436/vk-api/Modules/Auth/AuthViewController.swift
@@ -29,15 +29,24 @@ class AuthViewController: UIViewController {
                 urlComponents.scheme = "https"
                 urlComponents.host = "oauth.vk.com"
                 urlComponents.path = "/authorize"
-                urlComponents.queryItems = [
-                    URLQueryItem(name: "client_id", value: "7928655"),
-                    URLQueryItem(name: "display", value: "mobile"),
-                    URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
-                    URLQueryItem(name: "scope", value: "262150"),
-                    URLQueryItem(name: "response_type", value: "token"),
-                    URLQueryItem(name: "revoke", value: "1"),
-                    URLQueryItem(name: "v", value: "5.68")
-                ]
+//                urlComponents.queryItems = [
+//                    URLQueryItem(name: "client_id", value: "7928655"),
+//                    URLQueryItem(name: "display", value: "mobile"),
+//                    URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+//                    URLQueryItem(name: "scope", value: "262150"),
+//                    URLQueryItem(name: "response_type", value: "token"),
+//                    URLQueryItem(name: "revoke", value: "1"),
+//                    URLQueryItem(name: "v", value: "5.68")
+//                ]
+        urlComponents.queryItems = [
+                 URLQueryItem(name: "client_id", value: "7822904"),
+                 URLQueryItem(name: "display", value: "mobile"),
+                 URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+                 URLQueryItem(name: "scope", value: "262150"),
+                 URLQueryItem(name: "response_type", value: "token"),
+                 URLQueryItem(name: "revoke", value: "1"),
+                 URLQueryItem(name: "v", value: "5.68")
+             ]
                 
                 let request = URLRequest(url: urlComponents.url!)
                 
